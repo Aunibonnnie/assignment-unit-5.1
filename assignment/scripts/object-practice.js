a3d21329f87106cac0b6c0c9401f70b5e2da16b0
Aunika Lewis <Aunibonnie321@gmail.comgit config --global user.emailgit config --global user.email Aunibonnie123@gmail.com>
2024-10-13 14:14:49 -0500

    Completed assighment had a lot of studying with Codecademy about objects and template literals

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -8,8 +8,13 @@ console.log('***** Object Practice *****')
   - Give it  a numeric 'shoeCount' property
   - Also give it an array 'favThreeFoods'
 */
-const me = {
   // TODO - add properties here
+const me = {
+  firstName: 'Aunka',
+  lastName: 'Lewis',
+  hasSiblings: true,
+  shoeCount: 2,
+  favThreeFoods: ['Pizza', 'Sushi', 'Hamburger']
 };
 console.log('A little about me:', me);
 
@@ -19,23 +24,30 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
-
+const fullName = me.firstName + ' ' + me.lastName;
+console.log(fullName);
+// or use Template Literals
+console.log(`${me.firstName} ${me.lastName}`);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
-
-
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[2]);
+// or use Template Literals
+console.log(`${me.favThreeFoods[0]}`);
+console.log(`${me.favThreeFoods[2]}`);
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
+// plus one ++
+console.log(me.shoeCount);
+me.shoeCount++;
+console.log(me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +55,5 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = 'Purple';
+console.log(me.favoriteColor);
